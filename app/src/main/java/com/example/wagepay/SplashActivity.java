9d08ff22706a8408e5d4c110bb69767e94dd3f6a
Tirtha Rajbanshi <razztirtha@gmail.com>
2023-06-16 11:41:33 +0545

    initial commit

---
+++ app/src/main/java/com/example/wagepay/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.wagepay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    SharedPreferences introScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent iNext = new Intent(SplashActivity.this, IntroActivity.class);
+
+        new Handler().postDelayed(() -> {
+
+            introScreen = getSharedPreferences("introScreen",MODE_PRIVATE);
+            boolean isFirstTime = introScreen.getBoolean("firstTime",true);
+
+            if(isFirstTime){
+
+                SharedPreferences.Editor editor = introScreen.edit();
+                editor.putBoolean("firstTime", false);
+                editor.apply();
+
+            }
+            startActivity(iNext);
+            finish();
+
+
+        }, 4000);
+    }
+}
