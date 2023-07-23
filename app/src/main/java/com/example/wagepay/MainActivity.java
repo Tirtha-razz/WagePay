1d5c0f981975f2e039cd64498264036029c327d4
Tirtha Rajbanshi <razztirtha@gmail.com>
2023-07-23 10:53:19 +0545

    minor changes in login layout

--- app/src/main/java/com/example/wagepay/MainActivity.java
+++ app/src/main/java/com/example/wagepay/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wagepay;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
