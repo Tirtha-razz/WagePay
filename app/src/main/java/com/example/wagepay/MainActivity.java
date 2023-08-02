36cc9cf38a8b4fab7cc375b29b83b17e8652a1bd
Tirtha Rajbanshi <razztirtha@gmail.com>
2023-08-02 19:59:48 +0545

    returned to previous stage

--- app/src/main/java/com/example/wagepay/MainActivity.java
+++ app/src/main/java/com/example/wagepay/MainActivity.java
@@ -24,42 +24,44 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //defining variables
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     FloatingActionButton floatingActionButton;
+
     ArrayList<WorkRecyclerModel> arrWork = new ArrayList<>();
+
     BottomNavigationView bottomNavigationView;
 
     HomeFragment homeFragment = new HomeFragment();
     AttendanceFragment attendanceFragment = new AttendanceFragment();
+
     SummaryFragment summaryFragment = new SummaryFragment();
 
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set the status bar color
+        //set the status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.green));
         }
 
-        // Set bottom status bar color
+        //set bottom status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.green));
         }
+
+
 
         // Set the status bar icons' color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -67,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
 
-        // Set bottom status bar icons' color
+        //set bottom status bar icons' color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             View decorView = getWindow().getDecorView();
             int flags = decorView.getSystemUiVisibility();
@@ -75,34 +77,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             decorView.setSystemUiVisibility(flags);
         }
 
-        // Hooks
+        //hooks
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
         bottomNavigationView = findViewById(R.id.bottomNav);
 
-        // For home navigation clicked while app is opened
+        //for home navigation clicked while app is opened
         Menu menu = navigationView.getMenu();
         MenuItem homeMenuItem = menu.findItem(R.id.home); // Replace nav_home with the ID of your home menu item
         homeMenuItem.setChecked(true); // Set the home menu item as checked
 
-        // Toolbar
+        // toolbar
         toolbar.setTitle("Sudip Baral");
         setSupportActionBar(toolbar);
 
-        // Navigation drawer
+
+
+        //navigation drawer
         navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.openDrawer, R.string.closeDrawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-        // For bottom navigation and fragments
-        getSupportFragmentManager().beginTransaction().replace(R.id.FirstFragment, homeFragment).commit();
+
+
+        //for bottom navigation and fragments
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.FirstFragment,homeFragment).commit();
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @SuppressLint("NonConstantResourceId")
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
+            public boolean onNavigationItemSelected( MenuItem item) {
                 if (item.getItemId() == R.id.home) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.FirstFragment, homeFragment).commit();
                     floatingActionButton.setVisibility(View.VISIBLE); // Show FAB in HomeFragment
@@ -111,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     getSupportFragmentManager().beginTransaction().replace(R.id.FirstFragment, attendanceFragment).commit();
                     floatingActionButton.setVisibility(View.GONE); // Hide FAB in AttendanceFragment
                     return true;
-                } else if (item.getItemId() == R.id.summary) {
+                }else if (item.getItemId() == R.id.summary) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.FirstFragment, summaryFragment).commit();
                     floatingActionButton.setVisibility(View.GONE); // Hide FAB in SummaryFragment
                     return true;
@@ -119,11 +127,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 return false;
             }
         });
-        // Bottom navigation and fragment finish here
+        //bottom navigation and fragment finish here
 
-        // For recycler view of work
+        //for recycler view of work
         RecyclerView recyclerView = findViewById(R.id.work_recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this ,LinearLayoutManager.HORIZONTAL,false));
 
         arrWork.add(new WorkRecyclerModel("work 1"));
         arrWork.add(new WorkRecyclerModel("work 2"));
@@ -134,77 +142,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         arrWork.add(new WorkRecyclerModel("work 7"));
         arrWork.add(new WorkRecyclerModel("+"));
 
-        WorkRecyclerAdapter adapter = new WorkRecyclerAdapter(this, arrWork);
+        WorkRecyclerAdapter adapter = new WorkRecyclerAdapter(this,arrWork);
         recyclerView.setAdapter(adapter);
 
-        // For floating action button
+        //for floating action button
         floatingActionButton = findViewById(R.id.floatingButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start Activity 2
+
+
                 startActivity(new Intent(MainActivity.this, WorkerFormActivity.class));
             }
         });
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-
-        // Create the authentication state listener
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    // Check if the token is about to expire
-                    long tokenExpirationTime = user.getMetadata().getCreationTimestamp() + user.getMetadata().getLastSignInTimestamp() + TOKEN_EXPIRATION_DURATION;
-                    if (tokenExpirationTime > System.currentTimeMillis()) {
-                        // Token is still valid, continue using the app
-                        // Optionally, you can refresh the token here to extend the session duration
-                    } else {
-                        // Token has expired, log the user out
-                        FirebaseAuth.getInstance().signOut();
-                        // Redirect the user to the login screen
-                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                        finish();
-                    }
-                } else {
-                    // User is signed out, navigate to the login screen
-                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                    finish();
-                }
-            }
-        };
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        // Start listening for authentication state changes
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Stop listening for authentication state changes to avoid memory leaks
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
             drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
+        }else {
             super.onBackPressed();
         }
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
         // Handle item selection here
         int itemId = item.getItemId();
         if (itemId == R.id.home) {
@@ -218,7 +185,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return false;
     }
-
-    // Replace this value with your desired token expiration duration in milliseconds
-    private static final long TOKEN_EXPIRATION_DURATION = 3600000; // 1 hour (3600 seconds * 1000 milliseconds)
 }
--- app/src/main/java/com/example/wagepay/SplashActivity.java
+++ app/src/main/java/com/example/wagepay/SplashActivity.java
@@ -1,9 +1,7 @@
 package com.example.wagepay;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,26 +9,26 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    SharedPreferences introScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Set the status bar color
+        //set the status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.green));
         }
 
-        // Set bottom status bar color
+        //set bottom status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.green));
         }
@@ -41,7 +39,7 @@ public class SplashActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
 
-        // Set bottom status bar icons' color
+        //set bottom status bar icons' color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             View decorView = getWindow().getDecorView();
             int flags = decorView.getSystemUiVisibility();
@@ -49,21 +47,29 @@ public class SplashActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(flags);
         }
 
-        mAuth = FirebaseAuth.getInstance();
 
         new Handler().postDelayed(() -> {
-            FirebaseUser currentUser = mAuth.getCurrentUser();
 
-            Intent iNext;
-            if (currentUser != null) {
-                // User is logged in
-                iNext = new Intent(SplashActivity.this, MainActivity.class);
-            } else {
-                // User is not logged in
-                iNext = new Intent(SplashActivity.this, LoginActivity.class);
+            introScreen = getSharedPreferences("introScreen",MODE_PRIVATE);
+            boolean isFirstTime = introScreen.getBoolean("firstTime",true);
+
+            if(isFirstTime){
+
+                SharedPreferences.Editor editor = introScreen.edit();
+                editor.putBoolean("firstTime",false);
+                editor.apply();
+
+                Intent iNext = new Intent(SplashActivity.this, IntroActivity.class);
+                startActivity(iNext);
+                finish();
+
+            }else {
+
+                Intent iNext = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(iNext);
+                finish();
             }
-            startActivity(iNext);
-            finish();
-        }, 3000);
+
+        },3000);
     }
 }
