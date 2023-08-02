51aa25bf0075e13327cd8959eceda8e24e62e9f3
Tirtha Rajbanshi <razztirtha@gmail.com>
2023-08-02 14:20:53 +0545

    user token expiry handling completed

--- app/src/main/java/com/example/wagepay/SplashActivity.java
+++ app/src/main/java/com/example/wagepay/SplashActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,23 +11,26 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
 
-    SharedPreferences introScreen;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //set the status bar color
+        // Set the status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.green));
         }
 
-        //set bottom status bar color
+        // Set bottom status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.green));
         }
@@ -39,7 +41,7 @@ public class SplashActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
 
-        //set bottom status bar icons' color
+        // Set bottom status bar icons' color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             View decorView = getWindow().getDecorView();
             int flags = decorView.getSystemUiVisibility();
@@ -47,29 +49,21 @@ public class SplashActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(flags);
         }
 
+        mAuth = FirebaseAuth.getInstance();
 
         new Handler().postDelayed(() -> {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
 
-            introScreen = getSharedPreferences("introScreen",MODE_PRIVATE);
-            boolean isFirstTime = introScreen.getBoolean("firstTime",true);
-
-            if(isFirstTime){
-
-                SharedPreferences.Editor editor = introScreen.edit();
-                editor.putBoolean("firstTime",false);
-                editor.apply();
-
-                Intent iNext = new Intent(SplashActivity.this, IntroActivity.class);
-                startActivity(iNext);
-                finish();
-
-            }else {
-
-                Intent iNext = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(iNext);
-                finish();
+            Intent iNext;
+            if (currentUser != null) {
+                // User is logged in
+                iNext = new Intent(SplashActivity.this, MainActivity.class);
+            } else {
+                // User is not logged in
+                iNext = new Intent(SplashActivity.this, LoginActivity.class);
             }
-
+            startActivity(iNext);
+            finish();
         }, 3000);
     }
 }
